c11ec3fc80e97dc09fe9a3656fc677a46f775993
Roger Studner <roger.studner@netuitive.com>
2014-12-03 12:53:38 -0500

    latest

---
+++ app/components/calculator-button.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+var CalculatorButtonComponent = Ember.Component.extend({
+  tagName: "div",
+  classNames: ['calculator-button'],
+  symbol: "&#61573;",
+  buttonSymbol: function() {
+    var v = this.get('symbol');
+    return new Handlebars.SafeString(v);
+  }.property('symbol')
+});
+
+export default CalculatorButtonComponent;
+g
