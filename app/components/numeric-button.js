ad3bc33be48032b7941eae0d60c6494a670f83b0
Roger Studner <roger.studner@netuitive.com>
2014-12-03 12:53:38 -0500

    latest

---
+++ app/components/numeric-button.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+var NumericButtonComponent = Ember.Component.extend({
+  tagName: "div",
+  classNames: ['calculator-button']
+});
+
+export default NumericButtonComponent;
---
+++ app/components/operation-button.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+var OperationButtonComponent = Ember.Component.extend({
+  tagName: "div",
+  classNames: ['calculator-button']
+});
+
+export default OperationButtonComponent;
