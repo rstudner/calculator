c7b38fe3ea2b39b37b05f178f961e3f9531e0a87
Roger Studner <roger.studner@netuitive.com>
2014-12-04 15:38:06 -0500

    tests passing, some css3 animations

--- tests/unit/controllers/calculator/index-test.js
+++ tests/unit/controllers/calculator/index-test.js
@@ -5,7 +5,7 @@ import {
 
 moduleFor('controller:calculator/index', 'CalculatorIndexController', {
   // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: ['controller:calculator']
 });
 
 // Replace this with your real tests.
--- tests/unit/routes/calculator/fancy-test.js
+++ tests/unit/routes/calculator/fancy-test.js
@@ -3,7 +3,7 @@ import {
   test
 } from 'ember-qunit';
 
-moduleFor('route:fancy', 'FancyRoute', {
+moduleFor('route:calculator/fancy', 'CalculatorFancyRoute', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
